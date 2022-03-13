1e038a86325089d7b5f74a1c1031a35d8ddfc660
JeonChangMin <imsameperson@gmail.com>
2022-03-13 18:24:54 +0900

    Fix bug

--- EdgeServer/src/main/java/hcs/offloading/edgeserver/datatypes/MockProfiles.java
+++ EdgeServer/src/main/java/hcs/offloading/edgeserver/datatypes/MockProfiles.java
@@ -2,13 +2,13 @@ package hcs.offloading.edgeserver.datatypes;
 
 public class MockProfiles {
 
-    public static int PERSON_THRESHOLD = 10000;
-    public static int CLASS_AGNOSTIC_THRESHOLD = 15000;
+    public static int PERSON_THRESHOLD = 100;
+    public static int CLASS_AGNOSTIC_THRESHOLD = 150;
 
     public static int get_profile(String labelName) {
         if (labelName == null) {
             return CLASS_AGNOSTIC_THRESHOLD;
-        } else if (labelName.equals("person") {
+        } else if (labelName.equals("person")) {
             return PERSON_THRESHOLD;
         } else {
             throw new IllegalArgumentException("Wrong labelName!");
