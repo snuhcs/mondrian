afbc0026a6fd59c35781139683cda7e4d382afd7
creamchoco3080 <ttotto3080@gmail.com>
2022-03-13 16:59:56 +0900

    Use if inside MockProfiles
    Co-authored-by: Changmin Jeon <12844401+JeonChangMin@users.noreply.github.com>

--- EdgeServer/src/main/java/hcs/offloading/edgeserver/datatypes/MockProfiles.java
+++ EdgeServer/src/main/java/hcs/offloading/edgeserver/datatypes/MockProfiles.java
@@ -6,11 +6,12 @@ public class MockProfiles {
     public static int CLASS_AGNOSTIC_THRESHOLD = 15000;
 
     public static int get_profile(String labelName) {
-        switch (labelName != null ? labelName : "NULL") {
-            case "person":
-                return PERSON_THRESHOLD;
-            default:
-                return CLASS_AGNOSTIC_THRESHOLD;
+        if (labelName == null) {
+            return CLASS_AGNOSTIC_THRESHOLD;
+        } else if (labelName.equals("person") {
+            return PERSON_THRESHOLD;
+        } else {
+            throw new IllegalArgumentException("Wrong labelName!");
         }
     }
 }
