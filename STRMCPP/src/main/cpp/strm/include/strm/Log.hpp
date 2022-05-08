55b9f41381d90cf8b69d6bbaf5ae148f3ee53443
JeonChangMin <imsameperson@gmail.com>
2022-05-08 20:45:09 +0900

    Swithable internal log

--- STRMCPP/src/main/cpp/strm/include/strm/Log.hpp
+++ STRMCPP/src/main/cpp/strm/include/strm/Log.hpp
@@ -4,10 +4,21 @@
 #include <android/log.h>
 
 #define TAG "STRMCPP"
+
+//#define INTERNAL_LOG // Log internal or not
+
+#ifdef INTERNAL_LOG
 #define LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
 #define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#else
+#define LOGV(...)  do {} while(0)
+#define LOGD(...)  do {} while(0)
+#define LOGI(...)  do {} while(0)
+#define LOGW(...)  do {} while(0)
+#define LOGE(...)  do {} while(0)
+#endif
 
 #endif // LOG_HPP_
