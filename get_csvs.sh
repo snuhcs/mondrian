ca59263c7fc0308cb37e0a26421b287ec73d2064
Seonjun Kim <cyanide17@snu.ac.kr>
2022-10-26 20:04:33 +0900

    Add script to get csvs; Ignore csvs/

---
+++ get_csvs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dirname="$(date '+%Y-%m-%d_%H-%M-%S')_h$(openssl rand -hex 2)"
+
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/roi.csv > csvs/roi.csv
+r1=$?
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/exec.csv > csvs/exec.csv
+r2=$?
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/boxes.txt > csvs/boxes.txt
+r3=$?
+if [ $r1 -eq 0 ] || [ $r2 -eq 0 ] || [ $r3 -eq 0] 
+then
+    mkdir csvs/$dirname
+    mv csvs/roi.csv csvs/$dirname/roi.csv
+    mv csvs/exec.csv csvs/$dirname/exec.csv
+    mv csvs/boxes.txt csvs/$dirname/boxes.txt
+    echo "STATUS: roi.csv $r1 | exec.csv $r2 | boxes.txt $r3"
+    echo "SAVED AT csvs/$dirname"
+fi
