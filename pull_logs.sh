78fc4cf3018816e4a3b3c37cdf921a87d1cb6d8b
Seonjun Kim <cyanide17@snu.ac.kr>
2022-10-26 20:31:44 +0900

    Refine pull_logs.sh

---
+++ pull_logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+base="logs"
+dir="$(date '+%Y-%m-%d_%H-%M-%S')_h$(openssl rand -hex 2)"
+
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/roi.csv > $base/roi.csv
+r1=$?
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/exec.csv > $base/exec.csv
+r2=$?
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/boxes.txt > $base/boxes.txt
+r3=$?
+
+if [ $r1 -eq 0 ]
+then
+    mkdir -p $base/$dir
+    mv $base/roi.csv $base/$dir/roi.csv
+fi
+
+if [ $r2 -eq 0 ]
+then
+    mkdir -p $base/$dir
+    mv $base/exec.csv $base/$dir/exec.csv
+fi
+
+if [ $r3 -eq 0 ]
+then
+    mkdir -p $base/$dir
+    mv $base/boxes.txt $base/$dir/boxes.txt
+fi
+
+if [ $r1 -eq 0 ] || [ $r2 -eq 0 ] || [ $r3 -eq 0 ]
+then
+    echo "status : roi.csv $r1 | exec.csv $r2 | boxes.txt $r3"
+    echo "saved at $base/$dir"
+fi
