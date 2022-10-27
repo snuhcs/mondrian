abc9553390488f237ac6ea5189360267011d4e73
Seonjun Kim <cyanide17@snu.ac.kr>
2022-10-27 10:20:25 +0900

    Change exec.csv -> timeline.csv in pull_logs.ps1

--- pull_logs.ps1
+++ pull_logs.ps1
@@ -1,3 +1,3 @@
 adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/roi.csv > roi.csv
-adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/exec.csv > exec.csv
+adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/timeline.csv > timeline.csv
 adb shell run-as hcs.offloading.strm cat /data/data/hcs.offloading.strm/boxes.txt > boxes.txt
