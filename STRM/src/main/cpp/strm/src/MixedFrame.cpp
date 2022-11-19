d7bd07a409ec4b5c21604afce2a4636727081453
Changmin Jeon <imsameperson@gmail.com>
2022-11-19 20:16:32 +0900

    Change background color of mixed frames (#129)
    * Change background color from white to gray
    
    * Add TODO comment

--- STRM/src/main/cpp/strm/src/MixedFrame.cpp
+++ STRM/src/main/cpp/strm/src/MixedFrame.cpp
@@ -7,9 +7,11 @@ namespace rm {
 int MixedFrame::numMixedFrames = 0;
 
 MixedFrame::MixedFrame(Device device, const std::set<RoI*>& packedRoIs, int mixedFrameSize)
-    : device(device), packedRoIs(packedRoIs), mixedFrameIndex(numMixedFrames++),
-      mixedFrameSize(mixedFrameSize) {
-  packedMat = cv::Mat::zeros(mixedFrameSize, mixedFrameSize, CV_8UC4);
+    : device(device), packedRoIs(packedRoIs), mixedFrameSize(mixedFrameSize),
+      packedMat(mixedFrameSize, mixedFrameSize, CV_8UC4, cv::Scalar(114, 114, 114, 255)),
+      mixedFrameIndex(numMixedFrames++) {
+  // TODO: Handle different background colors according to the model
+  // (e.g. white for YOLOv4, gray for YOLOv5)
   for (RoI* roi: packedRoIs) {
     assert(roi->isPacked());
     cv::Mat resizedMat = roi->getResizedMat();
