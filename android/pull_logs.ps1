da7a956617c91152c0c8e6f59eeee31dfd148e1d
ChangminJeon <imsameperson@gmail.com>
2023-05-12 05:37:10 +0900

    Arrange boxes.csv - roi.csv - frame.csv order in scripts

--- android/pull_logs.ps1
+++ android/pull_logs.ps1
@@ -1,4 +1,4 @@
 adb pull /data/local/tmp/config.json
+adb shell run-as hcs.offloading.mondrian cat /data/data/hcs.offloading.mondrian/boxes.csv > boxes.csv
 adb shell run-as hcs.offloading.mondrian cat /data/data/hcs.offloading.mondrian/roi.csv > roi.csv
 adb shell run-as hcs.offloading.mondrian cat /data/data/hcs.offloading.mondrian/frame.csv > frame.csv
-adb shell run-as hcs.offloading.mondrian cat /data/data/hcs.offloading.mondrian/boxes.csv > boxes.csv
