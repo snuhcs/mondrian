45202116d1962957e6fb82fe2c04362e44123307
ChangminJeon <imsameperson@gmail.com>
2023-06-28 15:11:01 +0900

    Support pretrained model

--- android/Mondrian/src/main/cpp/mondrian/src/Config.cpp
+++ android/Mondrian/src/main/cpp/mondrian/src/Config.cpp
@@ -121,7 +121,7 @@ MondrianConfig parseMondrianConfig(const std::string& jsonPath) {
 }
 
 void MondrianConfig::test() const {
-  std::set<std::string> datasets = {"virat", "mta"};
+  std::set<std::string> datasets = {"pretrained", "virat", "mta"};
 
   // Common
   if (EXECUTION_TYPE == FRAME_WISE_INFERENCE) {
@@ -132,14 +132,18 @@ void MondrianConfig::test() const {
   }
   assert(STREAM_MODE == roiExtractorConfig.STREAM_MODE);
   assert(FULL_FRAME_SIZE == inferenceEngineConfig.FULL_FRAME_SIZE);
-  assert(inferenceEngineConfig.DATASET == roiResizerConfig.DATASET);
 
   // ROIResizer
-  assert(datasets.find(roiResizerConfig.DATASET) != datasets.end());
-  assert(roiResizerConfig.PROBE_STEP_SIZE > 0);
-  assert(EXECUTION_TYPE == MONDRIAN || roiResizerConfig.NUM_PROBE_STEPS == 0);
+  if (roiResizerConfig.STATIC_SCALE) {
+    assert(roiResizerConfig.STATIC_TARGET_SCALE > 0);
+  } else {
+    assert(inferenceEngineConfig.DATASET == roiResizerConfig.DATASET);
+    assert(roiResizerConfig.NUM_PROBE_STEPS == 0 || roiResizerConfig.PROBE_STEP_SIZE > 0);
+    assert(EXECUTION_TYPE == MONDRIAN || roiResizerConfig.NUM_PROBE_STEPS == 0);
+  }
 
   // InferenceEngine
+  assert(datasets.find(inferenceEngineConfig.DATASET) != datasets.end());
   assert(!inferenceEngineConfig.DEVICES.empty());
   assert(!inferenceEngineConfig.INPUT_SIZES.empty());
   if (EXECUTION_TYPE == ROI_WISE_INFERENCE) {
