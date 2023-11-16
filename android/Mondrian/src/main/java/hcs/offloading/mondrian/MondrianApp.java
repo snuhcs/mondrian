eef367cb41568af001c9b784db09ab6ff7ade119
JeonChangMin <changmina141217@gmail.com>
2023-11-17 07:44:55 +0900

    Draw processor

--- android/Mondrian/src/main/java/hcs/offloading/mondrian/MondrianApp.java
+++ android/Mondrian/src/main/java/hcs/offloading/mondrian/MondrianApp.java
@@ -16,6 +16,8 @@ import org.json.JSONObject;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
@@ -165,13 +167,13 @@ public class MondrianApp implements VideoLoader.Callback, SurfaceHolder.Callback
 
     public void drawOutput(long rgbMatAddr, List<BoundingBox> results, long device) {
         Mat rgbMat = new Mat(rgbMatAddr);
-//        if (device == 1) {
-//            Imgproc.putText(rgbMat, "GPU", new Point(10, 80), 0, 3, new Scalar(0, 0, 255), 8);
-//        } else if (device == 2) {
-//            Imgproc.putText(rgbMat, "DSP", new Point(10, 80), 0, 3, new Scalar(255, 0, 0), 8);
-//        } else {
-//            assert(false);
-//        }
+        if (device == 1) {
+            Imgproc.putText(rgbMat, "GPU", new Point(10, 80), 0, 3, new Scalar(0, 0, 255), 8);
+        } else if (device == 2) {
+            Imgproc.putText(rgbMat, "DSP", new Point(10, 80), 0, 3, new Scalar(255, 0, 0), 8);
+        } else {
+            assert(false);
+        }
         Bitmap bitmap = Bitmap.createBitmap(rgbMat.cols(), rgbMat.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(rgbMat, bitmap);
         Bitmap outputBitmap = ImageUtils.drawBoxes(bitmap, results);
